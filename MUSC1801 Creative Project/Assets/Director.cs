612681c30afd2d7e31151c1af2cdab1852870499
hamhead105 <speedoheedo@hotmail.com>
2023-04-18 21:49:07 +1000

    added progressions to videos
    TODO adjust lengths

--- MUSC1801 Creative Project/Assets/Director.cs
+++ MUSC1801 Creative Project/Assets/Director.cs
@@ -12,6 +12,8 @@ public class Director : MonoBehaviour
     [SerializeField] private VideoPlayer interview1Video;
     [SerializeField] private VideoPlayer montageVideo;
 
+    [SerializeField] private bool hasFinishedFlag;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,55 +26,72 @@ public class Director : MonoBehaviour
         // Intro 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 1;
         }
 
         // What Kinda Music
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 2;
         }
 
         // Living Room Intro
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 3;
         }
 
         // Yussef Dayes Interview
         else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 4;
         }
 
         // Sensational
         else if (Input.GetKeyDown(KeyCode.Alpha5))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 5;
         }
 
         // Living Room 2 
         else if (Input.GetKeyDown(KeyCode.Alpha6))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 6;
         }
 
         // Zutomayo Intro
         else if (Input.GetKeyDown(KeyCode.Alpha7))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 7;
         }
 
         // Kansaete Kuyashiiwa
         else if (Input.GetKeyDown(KeyCode.Alpha8))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 8;
         }
 
         // Conclusion
         else if (Input.GetKeyDown(KeyCode.Alpha9))
         {
+            stateAnimator.SetTrigger("UseSkip");
             movieState = 9;
+        }
+
+        if (hasFinishedFlag)
+        {
+            print("finish movie");
+            movieState++;
+            stateAnimator.SetTrigger("UseSkip");
+            hasFinishedFlag = false;
         }
 
         stateAnimator.SetInteger("DirectorState", movieState);
